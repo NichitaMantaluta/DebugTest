eabef644d11ee352decdb0f51314eadfb537ff6b
F248 Master <f248master@champlaincollege.qc.ca>
2019-08-26 14:07:12 -0400

    Add project files.

---
+++ DebugTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 0;
+            int result = MakeComplicatedCalculation(a, b);
+            Console.WriteLine(result);
+        }
+
+        static int MakeComplicatedCalculation(int a, int b)
+        {
+            try
+            {
+                return a / b;
+            }
+
+            catch (System.DivideByZeroException)
+            {
+                Console.WriteLine("Error. You cannot divide by zero");
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Unknown Error");
+            }
+
+            return 0;
+
+        }
+    }
+}
